a27ec7efed6cd00668f8c311b730a6892e7c059c
William Clark <Bill.ironhand@gmail.com>
2023-02-09 18:35:32 -0600

    Added Rook piece rules

---
+++ Engine/Rules/OwnDestRule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Chess.Engine.Rules;
+
+internal class OwnOriginRule : Rule
+{
+
+    internal override bool IsValid(GameState gs, Move m, out string reason)
+    {
+        reason = "";
+        if (m.Type != MoveType.Standard && m.Type != MoveType.EnPassant)
+        {
+            return true;
+        }
+
+        if (base.OriginPieceIs(gs, m, Piece.Player1) != gs.IsPlayer1)
+        {
+            reason = "Starting square must have a piece you own";
+            return false;
+        }
+
+        return true;
+    }
+}
+
